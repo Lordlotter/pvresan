2cc00bdfb954a999729edd2b242540b14db5e88f
Lordlotter <Joojolord@Gmail.com>
2016-09-11 19:48:03 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '**TOKEN**');
+define('BOT_TOKEN', '**248623715:AAFE4ksg2BC_DhXUe8jbmvvFHxrY0t5NblU**');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
